6abb09c92bf0a01182efd3e880f39a7302595975
Nicholas2104 <nicholasnogueria.costa@gmail.com>
2024-06-05 12:50:58 -0300

    changing language selection

--- Forca/main.py
+++ Forca/main.py
@@ -1,15 +1,8 @@
 """Main"""
 import ai_word
 import interface
-# Begin by allowing user to select language and handling errors
-while True:
-    language_setting = input("Select language setting (ENG or PORT) ").upper()
-    if language_setting != "ENG" and language_setting != "PORT":
-        print(f"{language_setting} is and Invalid setting!!\n")
-    else:
-        break
 
-random_word = ai_word.generate_word(language_setting)
+random_word = ai_word.generate_word()
 guessed_letters = []
 
 interface.show_progress(random_word,guessed_letters)
