32e67c0fbabb88b948d7408c5592fa9c61beea3b
Nicholas2104 <nicholasnogueria.costa@gmail.com>
2024-06-05 14:07:44 -0300

    Fixes to ASCII aesthetics and method functionality show_game_state shows lives & guesses

--- Forca/interface.py
+++ Forca/interface.py
@@ -1,13 +1,13 @@
 """Interface e Progresso do Jogo"""
-def show_progress(random_word, guessed_letters):
+def show_guess(random_word, guessed_letters):
     progress = "_"*len(random_word)
     for index,each_letter in enumerate(random_word):
         if each_letter in guessed_letters:
             temp_progress = list(progress)
             temp_progress[index]=each_letter
             progress = "".join(temp_progress)
-    print(progress)
-def show_lives(lives):
+    print(f"Progress: {progress}\nGuessed Letters: {guessed_letters}")
+def show_game_state(lives,random_word,guessed_letters):
     HANGMAN_ASCII = ['''
       +---+
           |
@@ -45,4 +45,6 @@ def show_lives(lives):
      / \  |
          ===''']
     print(HANGMAN_ASCII[6-lives]+"\n")
-    print(f"You have {lives} lives left")
+    show_guess(random_word, guessed_letters)
+    print(f"You have {lives} lives left!!!")
+show_game_state(3,"apple",['p','l','g','b'])
